e2459954a97b4549362b65cc91b4051a85162e66
muwon <b379514@hanyang.ac.kr>
2025-09-24 16:07:15 +0900

    Revert "feat: 결제 시스템 임시커밋"
    This reverts commit e75bf9fedc45259f6ea0b608341f1e7b79cde998.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import './globals.css'
-import PortoneProvider from '@/components/PortoneProvider'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,9 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body className="ios">
-        <PortoneProvider>
           {children}
-        </PortoneProvider>
       </body>
     </html>
   );
